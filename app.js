0f52430f9bfe3eb28d8248fc2496db9a13aa9428
migsilva89 <migdrum@gmail.com>
2022-04-25 00:23:33 +0200

    funcionality

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-alert("dei")
+
+let range = document.getElementById("customRange2")
+let toggle = document.getElementById("toggle")
+let views = document.getElementById("viewsNumber")
+let price = document.getElementById("price")
+let pageViews = ["10K","50K","100K","500K", "1M"]
+let perMonth = [8, 12, 16, 24, 36]
+let yearly = false
+
+range.addEventListener("input", function () {
+    updateResults()
+    views.innerHTML = pageViews[range.value]
+})
+
+toggle.addEventListener("change", function(){
+    yearly = !yearly
+    updateResults()
+})
+
+function updateResults() {
+    price.innerHTML = yearly ? perMonth[range.value] * 0.75 : perMonth[range.value]
+}
+
+let submitBtn = document.getElementById("submit")
+
+submit.addEventListener("click", function(){
+    alert("Here we go!")
+    window.location.reload();
+})
+
