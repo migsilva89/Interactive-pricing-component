339bb993a4d8856145a121f9f45e5ee42d7dc248
migsilva89 <migdrum@gmail.com>
2022-05-10 14:54:12 +0200

    mobile + funcionaltyy done

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,25 @@ module.exports = {
     './public/**/*.{html,js}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'slideBar': '#a5f3eb',
+        'slideBg': '#10d5c2',
+        'discountbg': '#feece7',
+        'discounttxt': '#ff8c66',
+        'ctaTxt': '#bdccff',
+        'white': '#ffffff',
+        'veryPaleBlue': '#fafbff',
+        'emptyBar': '#eaeefb',
+        'toggleBg': '#cdd7ee',
+        'txt': '#858fad',
+        'ctaBg':'#293356',
+      },
+      
+      fontFamily: {
+        'Manrope': ['Manrope', 'sans-serif'] 
+      },
+    },
   },
   variants: {},
   plugins: [],
